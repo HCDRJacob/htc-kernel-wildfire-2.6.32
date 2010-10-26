da5fdfe8b0333919e1bb31a24b9bce922dc315fb
Jacob Crawley <jak0b_2006@hotmail.co.uk>
2010-10-26 22:41:33 +0100

    Wifi fix, thanks to Kali

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 12 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#12 PREEMPT Tue Oct 26 22:29:38 BST 2010"
+#define LINUX_COMPILE_TIME "22:29:38"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "HCDR"
+#define LINUX_COMPILE_DOMAIN "Jacob-PC"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
--- include/linux/utsrelease.h
+++ include/linux/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.32.21-HCDRJacob-g539f729"
+#define UTS_RELEASE "2.6.32.21-HCDRJacob-g0155d15-dirty"
